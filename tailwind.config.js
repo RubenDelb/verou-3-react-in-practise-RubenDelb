3fb134919cb5235595cbcfd6a16bc6139776156c
Ruben Delbrassine <ruben.delbrassine@gmail.com>
2022-04-20 14:58:08 +0200

    Add tailwind and its configuration and setup

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'sunrise-img': "url('/src/assets/sunrise.jpg')"
+      }
+    },
+  },
+  plugins: [],
+}
