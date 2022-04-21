81eaff1a4960a7a2b33daf5e254b6ad9992f777f
Ruben Delbrassine <ruben.delbrassine@gmail.com>
2022-04-21 12:52:07 +0200

    Add searched location to localstorage + add a Navbar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import axios from "axios";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { Route, Routes } from "react-router-dom";
+import NavBar from "./Components/NavBar";
 import CurrentWeather from "./CurrentWeather";
+import HourlyWeather from "./HourlyWeather";
 
 const API_KEY = process.env.REACT_APP_OPENWEATHERMAP_API_KEY;
 
@@ -9,6 +12,11 @@ const App = () => {
   const [allData, setAllData] = useState({})
   const [location, setLocation] = useState('')
 
+    useEffect(() => {
+    const storedLocation = JSON.parse(localStorage.getItem("previousLocation"))
+    if (storedLocation) setLocation(storedLocation)
+  }, [])
+
   const API_URL = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=${API_KEY}`
 
   const searchLocation = (event) => {
@@ -16,20 +24,22 @@ const App = () => {
       axios.get(API_URL).then(response => {
         setCurrentData(response.data)
         console.log(response.data);
-        const API_URL_2 = `https://api.openweathermap.org/data/2.5/onecall?lat=${response.data.coord.lat}&lon=${response.data.coord.lon}&units=metric&exclude=minutely,hourly&appid=${API_KEY}`
+        const API_URL_2 = `https://api.openweathermap.org/data/2.5/onecall?lat=${response.data.coord.lat}&lon=${response.data.coord.lon}&units=metric&exclude=minutely&appid=${API_KEY}`
         axios.get(API_URL_2).then(data => {
           setAllData(data.data)
           console.log(data.data);
         })
         setLocation('')
+        localStorage.setItem("previousLocation", JSON.stringify(location))
       })
     }
   }
 
   return (
     <div className="w-full h-screen bg-sunrise-img bg-center bg-cover bg-no-repeat text-white">
-      <div className="search">
-        <input className="text-neutral-200 rounded-full bg-stone-900/50 py-1 px-2 focus:outline-none focus:ring focus:ring-stone-400" 
+      <NavBar />
+      <div className="my-4 text-center">
+        <input className="text-neutral-200 rounded-full bg-stone-900/50 py-2 px-3 focus:outline-none focus:ring focus:ring-stone-400" 
           type="text" 
           value={location} 
           onChange={event => setLocation(event.target.value)} 
