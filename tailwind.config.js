4472b0806e3bd2a321dd3ad928d7a187c2b82498
Ruben Delbrassine <ruben.delbrassine@gmail.com>
2022-04-24 20:42:44 +0200

    Add more backgrounds to display dynamicaly

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,12 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
-        'sunrise-img': "url('/src/assets/sunrise.jpg')"
+        'sunrise-img': "url('/src/assets/sunrise.jpg')",
+        'clear-img': "url('/src/assets/clear.jpg')",
+        'rain-img': "url('/src/assets/rain.jpg')",
+        'snow-img': "url('/src/assets/snow.jpg')",
+        'clouds-img': "url('/src/assets/clouds.jpg')",
+        'overcast-img': "url('/src/assets/overcast.jpg')",
       }
     },
   },
