29866b8721a22710cfae779216fed97a927b784d
Ruben Delbrassine <ruben.delbrassine@gmail.com>
2022-06-21 16:24:23 +0200

    Change all images to webp for better performance

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,12 +5,12 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
-        'sunrise-img': "url('/src/assets/sunrise.jpg')",
-        'clear-img': "url('/src/assets/clear.jpg')",
-        'rain-img': "url('/src/assets/rain.jpg')",
-        'snow-img': "url('/src/assets/snow.jpg')",
-        'clouds-img': "url('/src/assets/clouds.jpg')",
-        'overcast-img': "url('/src/assets/overcast.jpg')",
+        'sunrise-img': "url('/src/assets/sunrise.webp')",
+        'clear-img': "url('/src/assets/clear.webp')",
+        'rain-img': "url('/src/assets/rain.webp')",
+        'snow-img': "url('/src/assets/snow.webp')",
+        'clouds-img': "url('/src/assets/clouds.webp')",
+        'overcast-img': "url('/src/assets/overcast.webp')",
       }
     },
   },
